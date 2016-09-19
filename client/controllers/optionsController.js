c4afc0af3e0d21004fc71ea1e00b7b4ac6063d87
Shray bansal <bansalshray@gmail.com>
2016-09-19 12:42:29 -0700

    added tables

--- client/controllers/optionsController.js
+++ client/controllers/optionsController.js
@@ -3,10 +3,21 @@ angular
   .controller('OptionsController', function ($scope, $location, FoodFactory, CalorieFactory) {
   if (!CalorieFactory.completed) $location.path('/');
 
+  $scope.formatted = {
+  	mcdonalds: "McDonalds",
+  	chipotle: "Chipotle",
+  	tacobell: "Taco Bell",
+  	subway: "Subway",
+  	innout: "In N' Out",
+  	starbucks: "Starbucks"
+  }
+
   $scope.calories = CalorieFactory.getCalories();
   $scope.restaurants = FoodFactory.getData();
-  
 
+  $scope.formatRestaurant = function(unformatted) {
+  	return $scope.formatted[unformatted];
+  }
   // for (var i = 0; i < $scope.restaurants.length; i++) {
   // 	$scope.num = 0;
   // 	// console.log(i, $scope.restaurants[i].restaurant)
